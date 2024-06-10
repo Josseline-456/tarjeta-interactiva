dd87e2d21910b7fa333729b4c362efcc11519dde
josseline-456 <josselinefariasgallardo@gmail.com>
2024-06-10 17:48:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let precioBase = 400000
+let cantidad =0;
+let totalPagar=0;
+
+
+document.querySelector("#precio").innertHTML= precioBase;
+document.querySelector(".cantidad").innertHTML= cantidad;
+document.querySelector("#totalPagar").innertHTML= totalPagar;
